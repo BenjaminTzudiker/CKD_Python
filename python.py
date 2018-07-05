43e8c9d62106908804ce41afab7bfbbbeff07ab7
Benjamin Tzudiker <benjamintzudiker@MacBook-Air-5.local>
2018-07-05 14:49:11 -0700

    Added run query function.

--- python.py
+++ python.py
@@ -10,8 +10,19 @@ dbhost = "localhost"
 try:
     conn = psycopg2.connect("dbname='{name}' user='{user}' host='{host}' password='{pw}'".format(user = dbuser, pw = dbpass, name = dbname, host = dbhost))
 except:
-    print("Database connection failed.")
+    print("Database connection failed, check database information specified in main python file.")
+    print("Exiting...")
     sys.exit()
 
 # Create a cursor from the database connection
 cursor = conn.cursor()
+
+# Tries to run the query passed to the function
+def runQuery(query):
+    try:
+        cursor.execute("""{q}""".format(q = query))
+        return true
+    except:
+        print("Query execution failed for query:\n" + query)
+        return false
+    
