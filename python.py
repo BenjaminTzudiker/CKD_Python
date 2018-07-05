1e24da093d6255c9634dfd0b778572e3a8a4b7e4
Benjamin Tzudiker <benjamintzudiker@MacBook-Air-5.local>
2018-07-05 14:43:05 -0700

    Basic psycopg2 database connection.

--- python.py
+++ python.py
@@ -1,1 +1,17 @@
 import psycopg2
+
+# Set these values to those used by your database
+dbuser = "postgres"
+dbpass = "postgres"
+dbname = "ckd"
+dbhost = "localhost"
+
+# Attempt to connect to the database
+try:
+    conn = psycopg2.connect("dbname='{name}' user='{user}' host='{host}' password='{pw}'".format(user = dbuser, pw = dbpass, name = dbname, host = dbhost))
+except:
+    print("Database connection failed.")
+    sys.exit()
+
+# Create a cursor from the database connection
+cursor = conn.cursor()
