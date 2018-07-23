de668c9dd07073d05fe77f7a2969e69e9307599d
Benjamin Tzudiker <benjamintzudiker@MacBook-Air-5.local>
2018-07-23 10:37:56 -0700

    Attempted to remove BOM from batch scripts.

--- run.py
+++ run.py
@@ -13,13 +13,13 @@ cursor = connect(dbuser, dbpass, dbname, dbhost)
     for row in cursor.fetchall():
         print(row)"""
 
-primaryTableWhereStatement = "exists (select 1 from encounter e1 where z.patient_id = e1.patient_id and exists (select 1 from diagnosis d1 where d1.encounter_id = e1.encounter_id and ((d1.icd_code like 'N17%' and d1.icd_type = 10) or (d1.icd_code like '584%' and d1.icd_type = 9))))"
+"""primaryTableWhereStatement = "exists (select 1 from encounter e1 where z.patient_id = e1.patient_id and exists (select 1 from diagnosis d1 where d1.encounter_id = e1.encounter_id and ((d1.icd_code like 'N17%' and d1.icd_type = 10) or (d1.icd_code like '584%' and d1.icd_type = 9))))"
 setupAddPrimaryTable("patient", keyColumnName = "patient_id", where = primaryTableWhereStatement)
 setupAddSecondaryTable("encounter", keyColumnName = "patient_id", parentTableName = "patient", parentKeyColumnName = "patient_id")
-setupAddSecondaryTable("diagnosis", keyColumnName = "encounter_id", parentTableName = "encounter", parentKeyColumnName = "encounter_id")
+setupAddSecondaryTable("diagnosis", keyColumnName = "encounter_id", parentTableName = "encounter", parentKeyColumnName = "encounter_id")"""
 
-"""setupAddPrimaryTable("site_source", keyColumnName = "site_source")
-setupAddSecondaryTable("site_source_test", keyColumnName = "site_source", parentTableName = "site_source", parentKeyColumnName = "site_source")"""
+setupAddPrimaryTable("site_source", keyColumnName = "site_source")
+setupAddSecondaryTable("site_source_test", keyColumnName = "site_source", parentTableName = "site_source", parentKeyColumnName = "site_source")
 
 run(mode = "buffered")
 
